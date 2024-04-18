26ebf8c2cee17b58eb670cabea42034457b745ff
MrEmmet66 <lorrenc7@gmail.com>
2024-04-18 17:04:38 +0300

    Initial commit

---
+++ MvcHelloApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MvcHelloApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
